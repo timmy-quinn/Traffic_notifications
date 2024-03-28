ffc218b27404e2653b44d764038a0a618de8a96e
timmy-quinn <tquinn5301@gmail.com>
2024-03-28 13:43:22 -0400

    Testing again

--- main_map_notifications.py
+++ main_map_notifications.py
@@ -26,21 +26,20 @@ avoid = ["tolls"]
 
 directions_result = gmaps.directions("1578 Tremont St. Boston, MA`",
                                      "289 Great Rd. Acton, MA",
-                                     #waypoints = "140 Commonwealth Avenue Chestnut Hill, MA",
                                      mode="driving",
                                      departure_time=datetime.now(), 
                                      avoid = "tolls"
                                      )
 
 formatted_results  = json.dumps(directions_result, indent = 4)
-with open("route2.json", "w") as f:
+with open("standard_route.json", "w") as f:
     json.dump(directions_result, f)
 
-with open("route1.json", "r") as f:
+with open("standard_route.json", "r") as f:
     route1 = json.load(f)
 
 with open("route2.json", "r") as f:
-    route2 = json.load(f)
+    standard_route = json.load(f)
 
 
 waypoints_route1 = [step.get("end_location") for leg in route1[0]["legs"] for step in leg["steps"]]
