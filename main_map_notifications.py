a721147abaa6022d4d80be7ff86db99a7f97845a
timmy-quinn <tquinn5301@gmail.com>
2024-03-29 18:42:22 -0400

    Added popup notifications that trigger when traffic conditions are bad

--- main_map_notifications.py
+++ main_map_notifications.py
@@ -1,34 +1,39 @@
+#################### Imports ##################################
 import googlemaps
 from datetime import datetime
 from win10toast import ToastNotifier
 import json
 import csv
 
+#################### Global Variables #########################
 standard_route_file = 'standard_route.json'
 request_file = 'trip_details.json'
 current_route_file = 'current_route.json'
+
+################### Local Functions ###########################
+class popup: 
+    title = None 
+    main = None
+    duration = None
 
 def getAPIKey(file):
     with open(file, "r") as f: 
         API_KEY = f.read()
     return API_KEY
 
-# toaster = ToastNotifier()
-# toaster.show_toast("Commute", "A really good message", duration = 5)
-
 def loadJson(jsonFile): 
     with open(jsonFile, "r") as file: 
         contents = json.load(file)
     return contents
 
-def directionsGet(request_file, results_file = ''): 
+def directionsGet(request, results_file = ''): 
     gmaps = googlemaps.Client(request['api_key'])
     # Parameters to be passed to API
-    directions_result = gmaps.directions(trip['origin'],
-                                         trip['destination'],
-                                         mode=trip['mode'],
+    directions_result = gmaps.directions(request['origin'],
+                                         request['destination'],
+                                         mode=request['mode'],
                                          departure_time=datetime.now(), 
-                                         avoid = trip['avoid']
+                                         avoid = request['avoid']
                                          )
     if results_file == '': 
         return directions_result
@@ -41,37 +46,37 @@ def getWaypoints(directions):
     waypoints = [step.get("end_location") for leg in directions[0]["legs"] for step in leg["steps"]]
     return waypoints
 
-# #print(formatted_results)
-# distance = (directions_result[0]['legs'][0]['duration_in_traffic']['text'])
-# print("Duration in traffic: ", distance)
-
-#print(directions_result)
-
 #Print formatted json data
 def printJson(data):
     print(json.dumps(data, indent=4))
 
 def main(): 
     #standard_route = directionsGetStore(trip_details, standard_route_file)
-    
     standard_route = loadJson(standard_route_file)
     request = loadJson(request_file)
-    current_route = directionsGet(trip_details)
+    current_route = directionsGet(request)
     printJson(current_route)
     current_waypoints = getWaypoints(current_route)
     standard_waypoints = getWaypoints(standard_route)
     printJson(current_waypoints)
+    duration = current_route[0]['legs'][0]['duration_in_traffic']['value']/60
 
-    if current_waypoints == standard_waypoints: 
-        print("Take standard route home")
+    toaster = ToastNotifier()
+
+    if current_waypoints != standard_waypoints:
+        toaster.show_toast("Traffic Alert", request["alternate_msg"], duration = 5)
     else: 
-        print("Go home another way")
+        toaster.show_toast("Traffic Alert", request["standard_msg"], duration = 5)
+    
+    if (duration > (55)):
+        toaster.show_toast("Traffic Alert", f"Trip home is going to take {duration} minutes", duration = 5)
+    else: 
+        toaster.show_toast("Traffic Alert", "Traffic is not bad,", duration = 5)
 
-    #getDirections(trip_details, current_route)
-    #if compareRoute(standard_route, current_route): 
-        #print("They're the same!!!!")
-    #else:
-        #print("They're not the same!!!!")
+
+################## Run main #####################
+if __name__ == '__main__':
+    main()
 
 
-main()
+
